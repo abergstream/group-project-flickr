12203587dc7cd86120cf7b0c8353beabbc34a094
dylanR96 <dylan.ravenson@gmail.com>
2024-02-08 15:51:55 +0100

    Changed variable name and added comments

--- js/script.js
+++ js/script.js
@@ -2,26 +2,30 @@ let baseUrl = `https://api.flickr.com/services/rest`;
 let method = "flickr.photos.search";
 let text = "monkey";
 let currentPage = 6;
-let url = `${baseUrl}?api_key=${pubkey}&method=${method}&text=${text}&page=${currentPage}&format=json&nojsoncallback=1`;
+let apiUrl = `${baseUrl}?api_key=${pubkey}&method=${method}&text=${text}&page=${currentPage}&format=json&nojsoncallback=1`;
+
+//Created a variable from the captured div with Id imgContainer
 let imgContainer = document.getElementById("imgContainer");
 
 async function monkeySearch() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(apiUrl);
     const data = await response.json();
+
+    //Created a variable shortcut 
     const photos = data.photos;
     let currentPage = photos.page;
     let totalPages = photos.pages;
-    // let imgSize = "q";
-    // let url = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
-    console.log(data);
-    console.log(currentPage);
-    console.log(totalPages);
+
+    //Created a loop to loop all the photos from data.photos.photo
     for (const img of photos.photo) {
       let imgSize = "q";
-      let url = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
+      let imgUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
+      //Element is created and value is given to a variable
       const imgElement = document.createElement("img");
-      imgElement.src = url;
+      //Src of the img element is given the value of url
+      imgElement.src = imgUrl;
+      //Appending the new element to the div container
       imgContainer.appendChild(imgElement);
     }
   } catch (error) {
