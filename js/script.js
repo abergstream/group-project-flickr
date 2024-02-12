66999c6381f4a4955f53064ea62a1afd68a7aaa7
dylanR96 <dylan.ravenson@gmail.com>
2024-02-12 12:56:28 +0100

    Added HTML, CSS and javascript for lightbox effect, added most of script for lightbox

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-let baseUrl = `https://api.flickr.com/services/rest/`;
+let baseUrl = `https://api.flickr.com/services/rest`;
 let method = "flickr.photos.search";
 // Default amount of photos per page
 let photosPerPage = 20;
 let currentPage;
 let query;
+
+let photoArray;
+let prevIndex;
+let nextIndex;
 //Created variables for DOM-elements
 const imgContainer = document.getElementById("imgContainer");
 const searchBox = document.getElementById("search-box");
 const searchForm = document.getElementById("search-form");
-const imgPerPage = document.querySelector(".img-per-page");
+const imagePerPage = document.querySelector(".img-per-page");
+// Ta bort sen
+
+imagePerPage.addEventListener("change", () => {
+  photosPerPage = imagePerPage.value;
+  fetchImage(query, currentPage)
+})
+
 
 async function fetchImage(keyword, currentPage) {
   imgContainer.innerHTML = "";
-  // media=photos verkar ha fått bort felaktiga bilder, kanske APIn försökte läsa in videos i ett img-element
-  let apiUrl = `${baseUrl}?api_key=${pubkey}&method=${method}&text=${keyword}&page=${currentPage}&per_page=${photosPerPage}&format=json&nojsoncallback=1&media=photos`;
+  let apiUrl = `${baseUrl}?api_key=${pubkey}&method=${method}&text=${keyword}&page=${currentPage}&per_page=${photosPerPage}&format=json&nojsoncallback=1`;
 
   try {
     const response = await fetch(apiUrl);
@@ -24,12 +34,15 @@ async function fetchImage(keyword, currentPage) {
     // clears our images when we change the page or when we do a new search
     imgContainer.innerHTML = "";
     // Starts a loop and calls a function to load and append images to the HTML doc
-    for (const img of photos.photo) {
-      loadImage(img);
-    }
+    photoArray = photos.photo;
+    photos.photo.forEach((img, index) => {
+      loadImage(img, index);
+    });
+
   } catch (error) {
     console.error("Error: " + error);
   }
+
 }
 
 const prevPage = document.getElementById("button-prev-page");
@@ -40,35 +53,69 @@ const nextPage = document.getElementById("button-next-page");
 nextPage.addEventListener("click", () => {
   fetchImage(query, currentPage + 1);
 });
-// const firstPage = document.getElementById("button-first-page");
-// firstPage.addEventListener("click", () => {
-//   fetchImage(query, 1);
-// });
-// const lastPage = document.getElementById("button-last-page");
-// lastPage.addEventListener("click", () => {
-//   fetchImage(query, totalPages);
-// });
+const prevArrow = document.getElementById("prevArrow");
+prevArrow.addEventListener("click", () => {
+  openLightbox(photoArray[prevIndex], prevIndex);
+});
+const nextArrow = document.getElementById("nextArrow");
+nextArrow.addEventListener("click", () => {
+  openLightbox(photoArray[nextIndex], nextIndex);
+});
 
-function loadImage(img) {
+function loadImage(img, index) {
   let imgSize = "q";
   let imgUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
   //Element is created and value is given to a variable
   const imgElement = document.createElement("img");
-  const lotsOfImages = document.createElement("picture");
   //Src of the img element is given the value of url
   imgElement.src = imgUrl;
   //Appending the new element to the div container
   imgElement.setAttribute("alt", img.title);
-  lotsOfImages.appendChild(imgElement);
-  imgContainer.appendChild(lotsOfImages);
+  imgContainer.appendChild(imgElement);
+  imgElement.addEventListener("click", () => {
+    openLightbox(img, index, photoArray);
+  });
 }
+
+function openLightbox(img, index) {
+  prevIndex = index - 1;
+  nextIndex = index + 1;
+  const lightboxPhoto = document.getElementById("lightboxPhoto");
+  const lightboxThumbnail = document.getElementById("lightboxThumbnail");
+  const lightbox = document.querySelector(".lightbox");
+
+  const imgUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`;
+  lightboxPhoto.src = imgUrl;
+  console.log(img);
+
+  const lightboxThumb1 = createThumbnail(photoArray[prevIndex], prevIndex, photoArray);
+  const lightboxThumb2 = createThumbnail(img, index, photoArray);
+  lightboxThumb2.classList.add("active-thumb");
+  const lightboxThumb3 = createThumbnail(photoArray[nextIndex], nextIndex, photoArray);
+
+  lightboxThumbnail.innerText = "";
+  lightboxThumbnail.append(lightboxThumb1, lightboxThumb2, lightboxThumb3);
+  lightbox.style.display = "flex";
+}
+
+function createThumbnail(img, index, photoArray) {
+  const thumbUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_t.jpg`;
+  const thumb = document.createElement("img");
+  thumb.addEventListener("click", () => {
+    openLightbox(photoArray[index], index, photoArray);
+  })
+  thumb.src = thumbUrl;
+  return thumb;
+}
+
+
 
 function loadPagination(page, pages, keyword) {
   currentPage = page;
   query = keyword;
 
   const pageList = document.querySelector(".page-list");
-  console.log(pages);
+
   pageList.innerHTML = "";
   if (page > 3) {
     const listItemFirstPage = document.createElement("li");
@@ -96,17 +143,14 @@ function loadPagination(page, pages, keyword) {
   }
 }
 
-// Funktion att ändra photosPerPage när man ändrar value i selecten
-imgPerPage.addEventListener("change", () => {
-  photosPerPage = imgPerPage.value;
-  fetchImage(query, currentPage);
-});
+function searchImages() {
+  keyword = searchBox.value;
+  fetchImage(keyword, 1);
+}
 
-// Lade in fetchImage direkt i eventlistenern
-// la searchBox.value direkt i funktionen istället för variabel
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  fetchImage(searchBox.value, 1);
+  searchImages();
 });
 
 fetchImage("monkey", 1);
